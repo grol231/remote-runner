c8d42746c53fdd54953cdc44b2210c1d16e70275
Grigoriy Kosterev <kosgrimi@yandex.ru>
2017-02-05 14:13:39 +0300

    modify cmakelists

--- src/main.cpp
+++ src/main.cpp
@@ -13,14 +13,13 @@ public:
     {
         ip::tcp::endpoint ep(ip::tcp::v4(), 2001);
         ip::tcp::acceptor acc(io_service_, ep);
-        //char data[1024];
+        char data[1024];
         while(true)
         {
             server::socket_ptr sock(new ip::tcp::socket(io_service_));
             acc.accept(*sock);
-            //boost::thread( boost::bind(client_session, sock));
-           // int c = boost::asio::read(
-            //sock, data, boost::asio::buffer(data),boost::bind() );
+            boost::thread( boost::bind(client_session, sock));
+            int c = boost::asio::read(sock, data, boost::asio::buffer(data),boost::bind() );
         }
     }
     //void client_session()
