ee001c56893d07cd7e897771ee26db6e73a796ae
Grigoriy Kosterev <kosgrimi@yandex.ru>
2017-02-07 22:33:47 +0300

    begin

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <boost/asio.hpp>
+#include "server.h"
 
 using namespace boost::asio;
 
@@ -12,23 +13,28 @@ public:
     void run()
     {
         ip::tcp::endpoint ep(ip::tcp::v4(), 2001);
-        ip::tcp::acceptor acc(io_service_, ep);
-        //char data[1024];
+        ip::tcp::acceptor acc(this->io_service_, ep);
+        char data[1024];
         while(true)
         {
             server::socket_ptr sock(new ip::tcp::socket(io_service_));
             acc.accept(*sock);
-       
-            //int c = boost::asio::read(sock, data, boost::asio::buffer(data),boost::bind() );
+
+            int c = boost::asio::read(sock, data, boost::asio::buffer(data),boost::bind(&server::client_session,this) );
         }
     }
-    
+	void cleint_session(){
+		printf("Hello world!\n");
+	}
+
 private:
    boost::asio::io_service io_service_;
 };
 
 int main()
 {
+	//server s;
+	//s.run();
 	return 0;
 }
 
