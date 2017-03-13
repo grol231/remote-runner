f8d86f03d30863e3060842e36eafb5532c5fcaa0
grol231 <kosgrimi@gmail.com>
2017-03-13 10:10:43 +0200

    make linux daemon

--- src/main.cpp
+++ src/main.cpp
@@ -13,31 +13,33 @@ int main(int argc, char* argv[])
     //Error handling will be in log.
     try
     {
+        pid_t pid = fork();
+        if(pid < 0)
+        {
+            std::cout << "can't fork" << std::endl;            
+            exit(1);
+        }
+        else
+        {
+            if(pid != 0)            
+            {
+                std::cout << "I am in the parent." << std::endl;
+                exit(0);
+            }
+            else
+            {
+                std::cout << "I am in the child." << std::endl;
+            }                
+        }
+        setsid();
+        unsigned int numFiles = sysconf(_SC_OPEN_MAX);
+        for(unsigned int i = 0; i < numFiles; ++i)
+        {
+            close(i);
+        }
         std::unique_ptr<Config> config(new Config(argc, argv));
       //TODO:Use shared_ptr!
-      //Application app(config);
-        logging::add_common_attributes();
-        using namespace logging::trivial;
-        src::severity_logger<severity_level> lg;
-        Logging::Log log;
-        Logging::Statistic s;
-        Logging::LogRecord r;
-        s.StopedCommandCounter = 5;
-        s.NotRunningCommandCounter = 6;
-        s.CompletedCommandCounter = 7;
-        s.CompletedCompulsorilyCommandCounter = 8;
-        s.DownloadedData = 99;
-        s.UploadedData = 999;
-        r.Command = "gnome-terminal";
-        r.Condition = "start";
-        r.Result = "success";
-        r.Note = "This is good program.";
-       // LogType.set("Statistic");
-        BOOST_LOG_SEV(lg, trace) << Logging::ToString(s);
-        BOOST_LOG_SEV(lg, info) << Logging::ToString(s);
-       // LogType.set("LogRecord");  
-        BOOST_LOG_SEV(lg, fatal) << Logging::ToString(r);
-
+        Application app(config);
     }
     catch(boost::system::system_error& e)
     {
